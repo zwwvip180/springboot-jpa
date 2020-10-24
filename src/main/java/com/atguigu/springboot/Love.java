f96b3e720fde2f0eb29e49c03118d111e21843d8
zhouweiwu <761375570@qq.com>
2020-10-24 17:53:11 +0800

    修改了文件2222

--- src/main/java/com/atguigu/springboot/Love.java
+++ src/main/java/com/atguigu/springboot/Love.java
@@ -2,6 +2,6 @@ package com.atguigu.springboot;
 
 public class Love {
     public static void main(String[] args) {
-        System.out.println("111111");
+        System.out.println("1111112222");
     }
 }
